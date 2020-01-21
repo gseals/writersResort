7bff222d6f3e2e024622b951388195257121e357
Gabriel Seals <sealsgj@gmail.com>
2020-01-20 21:05:56 -0600

    firebase auth and login work

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,58 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Route, Redirect, Switch,
+} from 'react-router-dom';
 import './App.scss';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import firebaseConnection from '../helpers/data/connection';
+
+import Auth from '../components/pages/Auth/Auth';
+import AllPosts from '../components/pages/AllPosts/AllPosts';
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/auth', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+firebaseConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
     return (
     <div className="App">
-      <button className="btn btn-danger">This that test</button>
+      <Router>
+        <Switch>
+          <PrivateRoute path="/" exact component={AllPosts} authed={authed}/>
+          <PublicRoute path="/auth" exact component={Auth} authed={authed}/>
+          {/* <PrivateRoute path="/AllPosts" exact component={AllPosts} authed={authed}/> */}
+        </Switch>
+      </Router>
     </div>
     );
   }
