44b836c993a6b176483a4794c85164d84525502e
Gabriel Seals <sealsgj@gmail.com>
2020-01-21 21:58:42 -0600

    propz created

---
+++ src/helpers/propz/commentShape.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+const commentShape = PropTypes.shape({
+  postId: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  uid: PropTypes.string.isRequired,
+});
+
+export default { commentShape };
